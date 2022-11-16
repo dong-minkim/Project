954691197ddc874555dbeb33e8fc65779f444dbb
Dongmin Kim <kimdongmin68@naver.com>
2022-11-16 23:59:46 +0900

    feat(authserver): 예외 처리 시 상태 응답을 위한 ErrorCode ENUM 작성

---
+++ AuthServer/src/main/java/com/dutmdcjf/authserver/exception/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.dutmdcjf.authserver.exception;
+
+public enum ErrorCode {
+    LOGIN_BAD_REQUEST(401, null, "PLEASE LOGIN AGAIN"),
+    DO_NOT_MATCHING_TOKEN(403, null, "REFRESH TOKEN IS NOT SAME"),
+    NOT_FOUND_USER(404, null, "NOT FOUND USER"),
+    JWT_EXPIRED(405, null, "TOKEN HAS EXPIRED");
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
